92ec80c23b9c70add74e30c6ffffc6ac4d48d1f3
Harrison <hmseaborn@gmail.com>
2020-04-30 10:58:20 -0700

    hotfix i hope

--- utils/validateUser.js
+++ utils/validateUser.js
@@ -7,3 +7,13 @@ module.exports = (req, res, next) => {
          next();
      }
  }
+
+//  const validateUser = (req, res, next) => {
+//   if (!req.body) {
+//    res.status(400).json({ message: "Missing post data. Please ensure you sent the user's data." });
+//    } else if (!req.body.username || !req.body.password ) {
+//        res.status(400).json({ message: "Incomplete user data. Please include the user's username, password and role." });
+//    } else {
+//        next();
+//    }
+// }
